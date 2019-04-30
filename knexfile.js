bad33a35e27a32020984aa6fe8924c04c9f9b9c3
kallyl <kallyl_lacerda@hotmail.com>
2019-04-30 15:17:25 -0300

    Try heroku postgres db

--- knexfile.js
+++ knexfile.js
@@ -30,7 +30,7 @@ module.exports = {
         }
     },
     production: {
-        client: 'pg',
+        client: 'postgresql',
         connection: process.env.DATABASE_URL,
         migrations: {
             directory: __dirname + '/migrations',
